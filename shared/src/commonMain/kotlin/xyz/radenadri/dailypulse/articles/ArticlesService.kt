072b426b89c3e49837451a40568419bf2c463986
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-07-22 17:04:26 +0700

    feat: 4. Adding Ktor and fetching news from API

---
+++ shared/src/commonMain/kotlin/xyz/radenadri/dailypulse/articles/ArticlesService.kt
@@ -0,0 +1,19 @@
+package xyz.radenadri.dailypulse.articles
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class ArticlesService(private val httpClient: HttpClient) {
+
+    private val country = "us"
+    private val category = "business"
+    private val apiKey = "26790b21f4ea4a3aa10b5cf33aa91f7e"
+
+    suspend fun fetchArticles(): List<ArticleRaw> {
+        val response: ArticlesResponse =
+            httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey").body()
+
+        return response.articles
+    }
+}
---
+++ shared/src/commonMain/kotlin/xyz/radenadri/dailypulse/articles/ArticlesUseCase.kt
@@ -0,0 +1,40 @@
+package xyz.radenadri.dailypulse.articles
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+
+class ArticlesUseCase(private val service: ArticlesService) {
+    suspend fun getArticles() : List<Article> {
+        val articlesRaw = service.fetchArticles()
+        return mapArticles(articlesRaw)
+    }
+
+    private fun mapArticles(articlesRaw: List<ArticleRaw>): List<Article> = articlesRaw.map {
+        Article(
+            title = it.title,
+            desc = it.desc ?: "Click to find out more",
+            date = getDaysAgoString(it.date),
+            imageUrl = it.imageUrl ?: "https://picsum.photos/200/300"
+        )
+    }
+
+    private fun getDaysAgoString(date: String) : String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
+    }
+}
